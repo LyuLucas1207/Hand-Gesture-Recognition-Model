4bb579d7670b6dea5506ef96219deff6d10c8055
Lucas <lyuchongkai@gmail.com>
2024-11-02 22:01:16 -0700

    2024/11/02

--- collect_imgs.py
+++ collect_imgs.py
@@ -14,9 +14,9 @@ def collect_imgs (number_of_classes = [0, 1, 2], dataset_size=100):
         content = 'Press "S" to start collecting data for class {}'.format(folder)
         fts.show_text(cap, content)
 
-        max_number, _ = fts.get_max_list_image(IMAGE_DIR)
-        print('Max number: {}'.format(max_number))
-        fts.write_images(cap, max_number+1, dataset_size, DATA_DIR, folder)
+        max_images, _ = fts.get_max_list_image(IMAGE_DIR)
+        print('Max number: {}'.format(max_images))
+        fts.write_images(cap, max_images+1, dataset_size, DATA_DIR, folder)
 
     cap.release() # Release the VideoCapture object
     cv2.destroyAllWindows() # Close all OpenCV windows
